176ab3bf5d9afbe94040849f87937bd3a150a6d4
wind <2769323502@qq.com>
2025-05-26 17:45:56 +0800

    Setup basic Application and EntryPoint.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,19 @@
+#include "Application.h"
+
+
+namespace Hazel {
+	Application::Application() {
+		// Initialize the application
+	}
+
+	Application::~Application() {
+		// Clean up the application
+	}
+
+	void Application::Run() {
+		// Main loop of the application
+		while (true) {
+			// Process events, update, render, etc.
+		}
+	}
+}
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Core.h"
+
+namespace Hazel
+{
+	// Forward declaration of the Application class
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+		void Run();
+	};
+
+	// To be defined in CLIENT
+	Application* CreateApplication();
+}
+
