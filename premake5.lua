dbdae720260a92ca40313857a3fa35a6bd8b08c1
Wind <2769323502@qq.com>
2025-11-01 23:33:16 +0800

    add precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -19,6 +19,9 @@ project "Hazel"
     -- ÖÐ¼äÄ¿Â¼
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+    pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
+
     files { 
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp"
